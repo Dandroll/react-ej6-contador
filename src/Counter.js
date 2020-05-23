acd6b5adfb6d097bb74d2a7f5016d55427eebf27
Dandroll <daniel.osvaldo.truyol@gmail.com>
2020-05-23 13:36:56 -0300

    Contador

---
+++ src/Counter.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+
+
+
+
+const Counter = (props) => {
+    const [ counter, setCounter] =useState(0);
+    const updateCounter = props => setCounter(counter+5);
+
+    return (
+        <div className='ui container inverted segment black center'>
+            <h1 className='ui huge center aligned blue header' >{props.children}.-{props.author}</h1>
+            <div className='ui  huge container inverted segment'>
+                <h2 className='ui center aligned red header'>{counter}</h2>
+                <button onClick={updateCounter} className='ui button circular primary inverted blue big fluid ' tabindex='0'>Incrementar +5</button>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import Counter from './Counter';
+import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//import "./style.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  return (
+    <div className='container'>
+        <Counter author='Daniel Osvaldo Truyol'>Contador</Counter>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('root'));
